dcf511f38c3800783e2ce6fccf69e7a110b57288
carlosguevaran <carli3697@hotmail.com>
2014-08-31 12:00:29 -0500

    Manage Leds with speech recognition

---
+++ Client/speech.js
@@ -0,0 +1,80 @@
+var finalTranscript = '';
+var recognizing = false;
+var recognition = new webkitSpeechRecognition();
+var socket = io.connect("http://localhost:8001/");
+
+$(document).ready(function() {
+
+    // check that your browser supports the API
+    if (!('webkitSpeechRecognition' in window)){
+        alert("Sorry, your Browser does not support the Speech API");
+    } else{
+        // Create the recognition object and define the event handlers
+
+        socket.on();
+
+        recognition.continuous = true;         // keep processing input until stopped
+        recognition.interimResults = true;     // show interim results
+        recognition.lang = 'es-CO';           // specify the language
+
+        recognition.onstart = function() {
+            recognizing = true;
+            $('#instructions').html('Speak slowly and clearly');
+            $('#start_button').html('Click to Stop');
+        };
+
+        recognition.onerror = function(event) {
+            console.log("There was a recognition error...");
+        };
+
+        recognition.onend = function() {
+            recognizing = false;
+            $('#instructions').html('&nbsp;');
+        };
+
+        recognition.onresult = function(event) {
+            var interimTranscript = '';
+
+            // Assemble the transcript from the array of results
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    finalTranscript += event.results[i][0].transcript;
+                } else {
+                    interimTranscript += event.results[i][0].transcript;
+                }
+            }
+
+            // update the page and emit final transcript information
+
+            if(finalTranscript.length > 0) {
+                socket.emit('information', {
+                    'finalTranscript': finalTranscript,
+                    'sessionId': socket.socket.sessionId
+                });
+                console.log("final:    " + finalTranscript);
+                $('#transcript').html(finalTranscript);
+                recognition.stop();
+                $('#start_button').html('Click to Start Again');
+                recognizing = false;
+            }
+        };
+
+        $("#start_button").click(function(e) {
+            e.preventDefault();
+
+            if (recognizing) {
+                recognition.stop();
+                $('#start_button').html('Click to Start Again');
+                recognizing = false;
+            } else {
+                finalTranscript = '';
+                // Request access to the User's microphone and Start recognizing voice input
+                recognition.start();
+                $('#instructions').html('Allow the browser to use your Microphone');
+                $('#start_button').html('waiting');
+                $('#transcript').html('&nbsp;');
+            }
+        });
+    }
+   
+});
---
+++ Server/app.js
@@ -0,0 +1,59 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+var url = require('url');
+var speechText;
+var five = require("johnny-five");
+var board = new five.Board();
+var led;
+
+
+app.listen(8001);
+board.on("ready", function() {
+  console.log("speechText");
+});
+
+
+function handler (req, res) {
+  var path = url.parse(req.url).pathname;
+  console.log(path)
+  if (path == "/") {
+    res.writeHead(200);
+    res.write('Bienvenido! Empieza visitando index.html')
+    res.end();
+  }else if (path == "/index.html") {
+    fs.readFile(__dirname + path,
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+      res.writeHead(200);
+      res.end(data);
+    }); 
+  }else{
+    res.writeHead(404);
+    res.write('Error 404 Not Found');
+    res.end();
+  };
+};
+
+io.on('connection', function (socket) {
+  var x = "Saludos desde el servidor 8001";
+  console.log('a user connected');
+  socket.emit('news', x);
+  socket.on('information', function (data) {
+    console.log("Session ID: " + data.sessionId);
+    speechText = data.finalTranscript;
+    console.log(speechText);
+    led = new five.Led(13);
+    if(speechText == "prender"){
+      led.blink();
+    }else if(speechText == "apagar"){
+      console.log("AQUI")
+      led.stop();
+    }
+  });
+});
+
+
